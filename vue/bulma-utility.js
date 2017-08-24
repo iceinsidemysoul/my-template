d842be5bba8542a8a8c62750230dbc8afd16e8c8
iceinsidemysoul <sompong.paramon@gmail.com>
2017-08-24 13:47:04 +0700

    add tabs component

--- vue/bulma-utility.js
+++ vue/bulma-utility.js
@@ -1,1 +1,75 @@
-#for tabs
+var app = new Vue({
+
+	el: '#app',
+
+});
+
+// tabs component
+/*
+	usage
+	<tabs>
+      <tab name="menu1" :selected="true"></tab>
+      <tab name="menu2"></tab>
+      <tab name="menu3"></tab>
+  </tabs>
+*/
+Vue.component('tabs', {
+    template: `
+        <div>
+            <div class="tabs is-boxed">
+                <ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                        <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+                    </li>
+                </ul>
+                <slot></slot>
+            </div>
+        </div>
+    `,
+
+    data() {
+        return { tabs: [] };
+    },
+
+    created() {
+        this.tabs = this.$children;
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.href == selectedTab.href);
+            });
+        }
+    }
+});
+
+
+Vue.component('tab', {
+    template: `
+        <div><slot></slot></div>
+    `,
+
+    props: {
+        name: { required: true },
+        selected: { default: false }
+    },
+
+    data() {
+        return {
+            isActive: false
+        };
+    },
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted() {
+        this.isActive = this.selected;
+    },
+});
+
+
